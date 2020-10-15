504e80d6ef14a1502463cb3db2edec09a7c20a14
Reece Pieri <46065144+Icemaush@users.noreply.github.com>
2020-10-16 00:47:43 +0800

    Add files via upload

---
+++ CSVReadWrite.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace CSVReadWrite
+{
+    partial class CSVReadWrite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvOutput = new System.Windows.Forms.ListView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.lblRows = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tblMod = new System.Windows.Forms.TableLayoutPanel();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvOutput
+            // 
+            this.lvOutput.FullRowSelect = true;
+            this.lvOutput.HideSelection = false;
+            this.lvOutput.Location = new System.Drawing.Point(12, 12);
+            this.lvOutput.MultiSelect = false;
+            this.lvOutput.Name = "lvOutput";
+            this.lvOutput.Size = new System.Drawing.Size(824, 498);
+            this.lvOutput.TabIndex = 0;
+            this.lvOutput.UseCompatibleStateImageBehavior = false;
+            this.lvOutput.View = System.Windows.Forms.View.Details;
+            this.lvOutput.SelectedIndexChanged += new System.EventHandler(this.lvOutput_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(842, 13);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(99, 28);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load CSV";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(842, 47);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 28);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save CSV";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAs.Location = new System.Drawing.Point(842, 81);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(99, 28);
+            this.btnSaveAs.TabIndex = 3;
+            this.btnSaveAs.Text = "Save CSV As";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(842, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File:";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFile.Location = new System.Drawing.Point(842, 145);
+            this.lblFile.MaximumSize = new System.Drawing.Size(100, 0);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(0, 13);
+            this.lblFile.TabIndex = 5;
+            // 
+            // lblRows
+            // 
+            this.lblRows.AutoSize = true;
+            this.lblRows.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRows.Location = new System.Drawing.Point(898, 190);
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(0, 13);
+            this.lblRows.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(842, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Records:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(842, 275);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 28);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tblMod
+            // 
+            this.tblMod.AutoSize = true;
+            this.tblMod.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tblMod.ColumnCount = 1;
+            this.tblMod.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tblMod.Location = new System.Drawing.Point(3, 3);
+            this.tblMod.Name = "tblMod";
+            this.tblMod.RowCount = 2;
+            this.tblMod.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblMod.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblMod.Size = new System.Drawing.Size(0, 0);
+            this.tblMod.TabIndex = 9;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.Location = new System.Drawing.Point(842, 517);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(99, 75);
+            this.btnModify.TabIndex = 10;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.tblMod);
+            this.panel1.Location = new System.Drawing.Point(13, 517);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(823, 75);
+            this.panel1.TabIndex = 11;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(842, 493);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 12;
+            // 
+            // CSVReadWrite
+            // 
+            this.AcceptButton = this.btnModify;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 602);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblRows);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lvOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "CSVReadWrite";
+            this.Text = "CSV Read & Write";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvOutput;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label lblRows;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TableLayoutPanel tblMod;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ CSVReadWrite.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using LumenWorks.Framework.IO.Csv;
+
+
+namespace CSVReadWrite
+{
+    public partial class CSVReadWrite : Form
+    {
+        private string[] headers;
+        private string filePath;
+
+        public CSVReadWrite()
+        {
+            InitializeComponent();
+        }
+
+        // Load CSV file
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = Directory.GetCurrentDirectory();
+            openFile.Filter = "csv files (*.csv)|*.csv";
+
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFile.FileName;
+                ReadCSV(openFile.FileName);
+            }
+
+        }
+
+        // Save CSV file
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFile(filePath);
+        }
+
+        // Save CSV file as...
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFile.Filter = "csv files (*.csv)|*.csv";
+            saveFile.ShowDialog();
+
+            if (saveFile.FileName != "")
+            {
+                SaveFile(saveFile.FileName);
+            }
+        }
+
+        // Save file
+        private void SaveFile(string filePath)
+        {
+            using (StreamWriter outputFile = new StreamWriter(filePath))
+            {
+                outputFile.WriteLine(string.Join(",", headers));
+
+                foreach (ListViewItem item in lvOutput.Items)
+                {
+                    string[] fields = new string[item.SubItems.Count];
+                    for (int i = 0; i < item.SubItems.Count; i++)
+                    {
+                        fields[i] = item.SubItems[i].Text;
+                    }
+                    outputFile.WriteLine(string.Join(",", fields));
+                }
+            }
+
+            UpdateStatus("File saved.", Color.Red);
+        }
+
+        // Modify CSV record
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            ModifyRecord();
+        }
+
+        // Exit program
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Read CSV file
+        private void ReadCSV(string filename)
+        {
+            using (CsvReader csv = new CsvReader(new StreamReader(filename), true))
+            {
+                
+                int fieldCount = csv.FieldCount;
+                headers = csv.GetFieldHeaders();
+
+                // Add column headers to listview
+                foreach (string header in headers)
+                {
+                    lvOutput.Columns.Add(header);
+                }
+
+                while (csv.ReadNextRecord())
+                {
+                    var record = new ListViewItem();
+                    record.Text = csv[0];
+                    for (int i = 1; i < fieldCount; i++)
+                    {
+                        record.SubItems.Add(csv[i]);
+                    }
+                    lvOutput.Items.Add(record);
+                }
+            }
+            lvOutput.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            UpdateFileInfo();
+        }
+
+        // Update file info labels
+        private void UpdateFileInfo()
+        {
+            char[] separator = { '\\' };
+            string[] nameArray = filePath.Split(separator);
+            lblFile.Text = nameArray[nameArray.Length - 1];
+            lblRows.Text = lvOutput.Items.Count.ToString();
+        }
+
+        // Update modify table when an item is selected
+        private void lvOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateModifyTable();
+            ClearModifyTable();
+            UpdateStatus("", Color.Black);
+        }
+
+        // Add record to modify table
+        private void UpdateModifyTable()
+        {
+            if (lvOutput.SelectedItems.Count != 0)
+            {
+                tblMod.Controls.Clear();
+                tblMod.ColumnCount = headers.Length;
+                tblMod.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                tblMod.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    tblMod.Controls.Add(new Label() { Text = headers[i] }, i, 0);
+
+                }
+
+                for (int i = 0; i < lvOutput.SelectedItems[0].SubItems.Count; i++)
+                {
+                    tblMod.Controls.Add(new TextBox() { Text = lvOutput.SelectedItems[0].SubItems[i].Text }, i, 1);
+
+                    if (lvOutput.SelectedItems[0].SubItems[i].Text.Length > headers[i].Length)
+                    {
+                        lvOutput.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    }
+                }
+            }
+        }
+
+        // Modify record
+        private void ModifyRecord()
+        {
+
+            if (lvOutput.SelectedItems.Count != 0)
+            {
+                // Get collection of text boxes from modify table
+                List<Control> tblControls = new List<Control>();
+                foreach (Control ctrl in tblMod.Controls)
+                {
+                    if (ctrl is TextBox)
+                    {
+                        tblControls.Add(ctrl);
+                    }
+                }
+
+                // Update selected records in list view
+                int count = 1;
+                foreach (Control ctrl in tblControls)
+                {
+                    if (tblControls.IndexOf(ctrl) == 0)
+                    {
+                        lvOutput.SelectedItems[0].Text = ctrl.Text;
+                    }
+                    else
+                    {
+                        lvOutput.SelectedItems[0].SubItems[count].Text = ctrl.Text;
+                        count++;
+                    }
+                }
+
+                UpdateStatus("Record updated!", Color.Green);
+            }
+        }
+
+        // Clear modify table
+        private void ClearModifyTable()
+        {
+            if (lvOutput.SelectedItems.Count == 0)
+            {
+                foreach (Control ctrl in tblMod.Controls)
+                {
+                    if (ctrl is TextBox)
+                    {
+                        ctrl.Text = "";
+                    }
+                }
+            }
+        }
+
+        // Update status message
+        private void UpdateStatus(string message, Color color)
+        {
+            lblStatus.Text = message;
+            lblStatus.ForeColor = color;
+        }
+    }
+}
